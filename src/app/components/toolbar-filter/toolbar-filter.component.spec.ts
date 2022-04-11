504565c35daa0064b42ce7ad17ded545978cb5b7
Diego Romário <diego.romario@outlook.com>
2022-04-11 19:51:07 -0300

    toolbar filter component refactored and added unit test for filter event

--- src/app/components/toolbar-filter/toolbar-filter.component.spec.ts
+++ src/app/components/toolbar-filter/toolbar-filter.component.spec.ts
@@ -2,15 +2,18 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToolbarFilterComponent } from './toolbar-filter.component';
 
-describe('ToolbarFilterComponent', () => {
+const ANY_VALUE = 'any';
+describe(ToolbarFilterComponent.name, () => {
   let component: ToolbarFilterComponent;
   let fixture: ComponentFixture<ToolbarFilterComponent>;
+  let searchMethodName = ToolbarFilterComponent.prototype.search.name;
+  let spyFilterEvent: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ToolbarFilterComponent ]
+      declarations: [ToolbarFilterComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +22,17 @@ describe('ToolbarFilterComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    spyFilterEvent = spyOn(component.filterEvent, 'emit')
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`Given that ${searchMethodName} method is called, An event should be emitted sending 'value'`, () => {
+    component.value = ANY_VALUE
+    component.search();
+    expect(spyFilterEvent).toHaveBeenCalledOnceWith(ANY_VALUE);
   });
 });
--- src/app/components/toolbar-filter/toolbar-filter.component.ts
+++ src/app/components/toolbar-filter/toolbar-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'u13-toolbar-filter',
@@ -7,6 +7,8 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class ToolbarFilterComponent {
   value: string = '';
+  @Input() label: string;
+  @Input() placeHolder: string;
   @Output() public filterEvent = new EventEmitter<string>();
   search() {
     this.filterEvent.emit(this.value);
